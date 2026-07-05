d593b76c4083df595b872ded0e494ccdc25c4472
mayanaoguichimai-byte <mayanaogui@users.noreply.github.com>
2026-07-06 04:06:27 +0800

    configure nitro netlify preset for SSR deployment

---
+++ .netlify/functions-internal/server/_tanstack-start-manifest_v-1-9IdAmI.mjs
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/24135/.trae-cn/work/6a2bb7f3d270d25b0b649c58/project/app/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BYhy6lkJ.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BYhy6lkJ.js" } }] }, "/": { filePath: "C:/Users/24135/.trae-cn/work/6a2bb7f3d270d25b0b649c58/project/app/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Dcc1quna.js"] } } });
+export {
+  tsrStartManifest
+};
